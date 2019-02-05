dbdc7e02091f51e7fea13c7431ad899d42710cc0
GIERI <gieralt.artur@gmail.com>
2019-02-05 23:07:02 +0100

    roles, files

--- backend/src/models/FileModel.ts
+++ backend/src/models/FileModel.ts
@@ -2,9 +2,13 @@ import { Schema, model } from 'mongoose';
 
 export const FileSchema = new Schema({
   _id: Schema.Types.ObjectId,
-  name: { type: String, required: [true, 'Please provide a content'] },
-  type: { type: String, required: [true, 'Please provide a type'] },
-  path: { type: String, required: [true, 'Please provide a path'] },
+  originalname: { type: String, required: true },
+  encoding: { type: String, required: true },
+  mimetype: { type: String, required: true },
+  size: { type: String, required: true },
+  destination: { type: String, required: true },
+  filename: { type: String, required: true },
+  path: { type: String, required: true },
   uploadBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   created_date: {
     type: Date,
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,43 +2,52 @@ import { Application } from 'express';
 import * as ArticlesController from './controllers/ArticlesController';
 import * as CommentsController from './controllers/CommentsController';
 import * as UsersController from './controllers/UsersController';
-import passport = require('passport');
+import * as FilesController from './controllers/FilesController';
+import { authorize } from './auth/authorize';
 
 export class Routes {
   public routes(app: Application): void {
     app
       .route('/articles')
       .get(ArticlesController.getAll)
-      .post(ArticlesController.add);
+      .post(authorize(['Admin']), ArticlesController.add);
     app
       .route('/articles/:articleId')
       .get(ArticlesController.getSingle)
-      .put(ArticlesController.update)
-      .delete(ArticlesController.remove);
+      .put(authorize(['Admin']), ArticlesController.update)
+      .delete(authorize(['Admin']), ArticlesController.remove);
 
     app
       .route('/comments')
-      .get(CommentsController.getAll)
-      .post(CommentsController.add);
+      .get(authorize(), CommentsController.getAll)
+      .post(authorize(), CommentsController.add);
     app
       .route('/comments/:commentId')
-      .get(CommentsController.getSingle)
-      .put(CommentsController.update)
-      .delete(CommentsController.remove);
+      .get(authorize(), CommentsController.getSingle)
+      .put(authorize(), CommentsController.update)
+      .delete(authorize(), CommentsController.remove);
 
-    app
-      .route('/users')
-      .get(
-        passport.authenticate('jwt', { session: false }),
-        UsersController.getAll
-      );
+    app.route('/users').get(authorize(), UsersController.getAll);
     app.route('/register').post(UsersController.register);
     app.route('/login').post(UsersController.login);
 
     app
       .route('/users/:userId')
-      .get(UsersController.getSingle)
-      .put(UsersController.update)
-      .delete(UsersController.remove);
+      .get(authorize(), UsersController.getSingle)
+      .put(authorize(), UsersController.update)
+      .delete(authorize(), UsersController.remove);
+
+    app
+      .route('/files')
+      .get(authorize(['Admin']), FilesController.getAll)
+      .post(
+        authorize(['Admin']),
+        FilesController.FILE_UPLOAD_SETTINGS,
+        FilesController.upload
+      );
+    app
+      .route('/files/:fileId')
+      .get(authorize(['Admin']), FilesController.getSingle)
+      .delete(authorize(['Admin']), FilesController.remove);
   }
 }
